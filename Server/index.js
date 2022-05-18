99ac1712b9aefd53c4bf9b6a6b2bda8616c97a2b
Joshua West <JoshuaHWest@gmail.com>
2022-05-17 19:35:23 -0600

    created a basic server

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(4000, () => console.log('Server running on 4000'))
+
+
+
+app.get("/api/users", (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+  });
+
+  app.get("/weather/:temperature", (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today!!</h3>`;
+    res.status(200).send(phrase);
+  });
